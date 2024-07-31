78524fae4640f27a20056332cb0fb3cca247a015
Alex <71099375+SkyMantle@users.noreply.github.com>
2024-07-31 17:07:57 +0300

    resize change

--- src/assets/components/Blocks/MainExhibitions.jsx
+++ src/assets/components/Blocks/MainExhibitions.jsx
@@ -12,9 +12,9 @@ function MainExhibitions() {
 	)
 
 	function getPostsCount(Width) {
-		if (Width > 1919) {
+		if (Width > 1598 && Width > 1919) {
 			return 3
-		} else if (Width > 1440 && Width <= 1918) {
+		} else if (Width > 1440 && Width <= 1598) {
 			return 2
 		} else {
 			return 2
