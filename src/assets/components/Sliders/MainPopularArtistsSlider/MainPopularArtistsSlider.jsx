2c793d2696d20212986884534722c1e9c56ece4c
Vavadjan <vavadjan@gmail.com>
2024-08-08 00:00:44 +0300

    В процессе

--- src/assets/components/Sliders/MainPopularArtistsSlider/MainPopularArtistsSlider.jsx
+++ src/assets/components/Sliders/MainPopularArtistsSlider/MainPopularArtistsSlider.jsx
@@ -16,39 +16,14 @@ const Slide = () => {
 	const { t } = useTranslation()
 
 	return (
-		<div className={'${styles.mainPopularArtistsSliderContainer}'}>
-			<div className={'${styles.mainPopularArtistsSliderWrapper}'}>
-				<div className={'${styles.mainPopularArtistsSliderTopInnerWrapper}'}>
-					<div className={'${styles.mainPopularArtistsSliderTitleWrapper}'}>
-						<p className={'${styles.mainPopularArtistsSliderTitle}'}>{t('Популярне. "Мистецтво"')}
-
-						</p>
-					</div>
-					<div className={'${styles.mainPopularArtistsSliderLikeAndShareWrapper}'}>
-						<div className={'${styles.mainPopularArtistsSliderLikeInnerWrapper}'}>
-							<button className='socialLikeAndShareInner__likeButton circleButton'>
-								<img
-									className='likeButtonImg'
-									src='/Img/likeHeart.svg'
-									alt='Like' />
-							</button>
-						</div>
-						<div className={'${styles.mainPopularArtistsSliderShareInnerWrapper}'}>
-							<button className='socialLikeAndShareInner__shareButton circleButton'>
-								<img
-									className='shareButtonImg'
-									src='/Img/shareArrow.svg'
-									alt='Share' />
-							</button>
-						</div>
-					</div>
-				</div>
-
-				<div className={'${styles.mainPopularArtistsSliderBottomInnerWrapper}'}>
-
+		<div className={`${styles.mainPopularArtistsSliderCardWrapper}`}>
+			<div className={`${styles.mainPopularArtistsSliderCardInnerWrapper}`}>
+				<div className={`${styles.mainPopularArtistsSliderCardButtonWrapper}`}>
+					<button className={`${styles.mainPopularArtistsSliderCardButton}`}>
+						{t('Огляд')}
+					</button>
 				</div>
 			</div>
-
 		</div>
 	)
 }
@@ -56,49 +31,70 @@ const Slide = () => {
 const MainPopularArtistsSlider = () => {
 	const { t } = useTranslation()
 	return (
-		<div className={`${styles.mainPopularArtistsSliderContainer}`}>
+		<div className={'${styles.mainPopularArtistsSliderContainer}'}>
+			<div className={'${styles.mainPopularArtistsSliderWrapper}'}>
+				<div className={'${styles.mainPopularArtistsSliderTopInnerWrapper}'}>
+					<div className={'${styles.mainPopularArtistsSliderTitleWrapper}'}>
+						<p className={'${styles.mainPopularArtistsSliderTitle}'}>{t('Популярне. "Мистецтво"')}</p>
+					</div>
+					<div className={'${styles.mainPopularArtistsSliderLikeAndShareWrapper}'}>
+						<div className={'${styles.mainPopularArtistsSliderLikeInnerWrapper}'}>
+							<button className={'${styles.mainPopularArtistsSliderLikeButton}'}>
+								<img
+									className={'${styles.mainPopularArtistsSliderLikeButtonImg}'}
+									src='/public/Img/likeHeart.svg'
+									alt='Like' />
+							</button>
+						</div>
+						<div className={'${styles.mainPopularArtistsSliderShareInnerWrapper}'}>
+							<button className={'${styles.mainPopularArtistsSliderShareButtonButton}'}>
+								<img
+									className={'${styles.mainPopularArtistsSliderShareButtonImg}'}
+									src='/public/Img/shareArrow.svg'
+									alt='Share' />
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className={'${styles.mainPopularArtistsSliderBottomInnerWrapper}'}>
+					<Swiper
+						modules={[Navigation, Pagination]}
+						spaceBetween={0}
+						slidesPerView={'auto'}
+						navigation
+						pagination={{ clickable: false, type: 'fraction' }}
+						onSlideChange={() => console.log('slide change')}
+						onSwiper={swiper => console.log(swiper)}
+					>
+						<SwiperSlide>
+							<Slide />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Slide />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Slide />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Slide />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Slide />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Slide />
+						</SwiperSlide>
+						<SwiperSlide>
+							<Slide />
+						</SwiperSlide>
+					</Swiper>
+					<div className={'${swiper-button-prev}'}></div>
+					<div className={'${swiper-pagination}'}></div>
+					<div className={'${swiper-button-next}'}></div>
 
-
-
-
-			<Swiper
-				modules={[Navigation, Pagination]}
-				spaceBetween={0}
-				slidesPerView={'auto'}
-				navigation
-				pagination={{ clickable: false, type: 'fraction' }}
-				onSlideChange={() => console.log('slide change')}
-				onSwiper={swiper => console.log(swiper)}
-			>
-				<SwiperSlide>
-					<Slide />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Slide />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Slide />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Slide />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Slide />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Slide />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Slide />
-				</SwiperSlide>
-			</Swiper>
-			<div className={'${swiper-button-prev}'}></div>
-			<div className={'${swiper-pagination}'}></div>
-			<div className={'${swiper-button-next}'}></div>
-
-
-
-		</div>
+				</div>
+			</div>
+		</div>	
 	)
 }
 
