02302ff17c088e1a537838fe554ecdc8e61ac935
Vavadjan <vavadjan@gmail.com>
2024-08-19 12:00:08 +0300

    готово

--- src/assets/screens/userProfile/userProfilePosts.jsx
+++ src/assets/screens/userProfile/userProfilePosts.jsx
@@ -14,7 +14,7 @@ const UserProfile = () => {
 	}
 
 	const handleAddPostClick = () => {
-		navigate('/use.UserProfilePosts')
+		navigate('/use.userProfilePosts')
 	}
 	const handlePostsClick = () => {
 		navigate('/userProfilePosts')
@@ -43,12 +43,12 @@ const UserProfile = () => {
 				</button>
 			</div>
 			{/* Карточка публикации */}
-			<div className={`${styles.UserProfilePostsContainer}`}>
-				<div className={`${styles.UserProfilePostsWrapper}`}>
-					<div className={`${styles.UserProfilePostsPicAndTextWrapper}`}>
-						<div className={`${styles.UserProfilePostsPicWrapper}`}>
+			<div className={`${styles.userProfilePostsContainer}`}>
+				<div className={`${styles.userProfilePostsWrapper}`}>
+					<div className={`${styles.userProfilePostsPicAndTextWrapper}`}>
+						<div className={`${styles.userProfilePostsPicWrapper}`}>
 							<img
-								className={`${styles.UserProfilePostsPic}`}
+								className={`${styles.userProfilePostsPic}`}
 								src={'/Img/mainPopularArtistsSliderIMG.jpg'}
 								alt={t('Світлина мистецтва')}
 								onError={e => {
@@ -57,18 +57,18 @@ const UserProfile = () => {
 								}}
 							/>
 						</div>
-						<div className={`${styles.UserProfilePostsTextWrapper}`}>
-							<div className={`${styles.UserProfilePostsTitleWrapper}`}>
-								<h3 className={`${styles.UserProfilePostsTitle}`}>
+						<div className={`${styles.userProfilePostsTextWrapper}`}>
+							<div className={`${styles.userProfilePostsTitleWrapper}`}>
+								<h3 className={`${styles.userProfilePostsTitle}`}>
 									Нова публікація
 								</h3>
 							</div>
-							<div className={`${styles.UserProfilePostsDescriptionWrapper}`}>
-								<p className={`${styles.UserProfilePostsDescription}`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam maxime dolor ipsa. Ducimus, dignissimos. Fugit officia repellendus neque ducimus voluptate temporibus repudiandae quos doloribus ipsa consectetur modi, vitae, non minima!</p>
+							<div className={`${styles.userProfilePostsDescriptionWrapper}`}>
+								<p className={`${styles.userProfilePostsDescription}`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam maxime dolor ipsa. Ducimus, dignissimos. Fugit officia repellendus neque ducimus voluptate temporibus repudiandae quos doloribus ipsa consectetur modi, vitae, non minima!</p>
 							</div>
-							<div className={`${styles.UserProfilePostsClockDateAndButtonWrapper}`}>
-								<div className={`${styles.UserProfilePostsClockAndDateWrapper}`}>
-									<img className={`${styles.UserProfilePostsClock}`}
+							<div className={`${styles.userProfilePostsClockDateAndButtonWrapper}`}>
+								<div className={`${styles.userProfilePostsClockAndDateWrapper}`}>
+									<img className={`${styles.userProfilePostsClock}`}
 										src={'/Img/clock.svg'}
 										alt={t('Світлина мистецтва')}
 										onError={e => {
@@ -76,19 +76,19 @@ const UserProfile = () => {
 											e.target.src = '/Img/clock.svg'
 										}}
 									/>
-									<p className={`${styles.UserProfilePostsDate}`}>01.01.2022</p>
+									<p className={`${styles.userProfilePostsDate}`}>01.01.2022</p>
 								</div>
-								<button className={`${styles.UserProfilePostsButton}`}>{t('До публікації')}</button>
+								<button className={`${styles.userProfilePostsButton}`}>{t('До публікації')}</button>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div className={`${styles.UserProfilePostsContainer}`}>
-					<div className={`${styles.UserProfilePostsPicAndTextWrapper}`}>
-						<div className={`${styles.UserProfilePostsPicWrapper}`}>
+				<div className={`${styles.userProfilePostsWrapper}`}>
+					<div className={`${styles.userProfilePostsPicAndTextWrapper}`}>
+						<div className={`${styles.userProfilePostsPicWrapper}`}>
 							<img
-								className={`${styles.UserProfilePostsPic}`}
-								src={'/Img/mainPopularArtistsSliderIMG.jpg'}
+								className={`${styles.userProfilePostsPic}`}
+								src={'/Img/mainBanner.jpeg'}
 								alt={t('Світлина мистецтва')}
 								onError={e => {
 									e.target.onerror = null
@@ -96,22 +96,22 @@ const UserProfile = () => {
 								}}
 							/>
 						</div>
-						<div className={`${styles.UserProfilePostsTextWrapper}`}>
-							<div className={`${styles.UserProfilePostsTitleWrapper}`}>
-								<h3 className={`${styles.UserProfilePostsTitle}`}>
+						<div className={`${styles.userProfilePostsTextWrapper}`}>
+							<div className={`${styles.userProfilePostsTitleWrapper}`}>
+								<h3 className={`${styles.userProfilePostsTitle}`}>
 									Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius pariatur vero ducimus eveniet, aut adipisci at, eligendi magni perspiciatis facere saepe, sed architecto? Tenetur omnis aperiam harum tempore voluptas reiciendis.
 								</h3>
 							</div>
-							<div className={`${styles.UserProfilePostsDescriptionWrapper}`}>
-								<p className={`${styles.UserProfilePostsDescription}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae id optio officiis. Nam, eius ipsam ea ipsum culpa veniam blanditiis nobis magni dignissimos a debitis, aut in iste distinctio obcaecati!
+							<div className={`${styles.userProfilePostsDescriptionWrapper}`}>
+								<p className={`${styles.userProfilePostsDescription}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae id optio officiis. Nam, eius ipsam ea ipsum culpa veniam blanditiis nobis magni dignissimos a debitis, aut in iste distinctio obcaecati!
 									Ad, soluta repellat rem, impedit commodi doloribus saepe maxime harum minima blanditiis, consectetur corporis dolore in quod doloremque eos perspiciatis. Vitae sequi nostrum a quasi neque hic aut, veniam provident.
 									Vero voluptate, blanditiis provident quasi enim distinctio ipsa ducimus reiciendis laboriosam! Repudiandae minus, omnis, dolore incidunt officia hic doloremque sequi exercitationem natus, dolorem earum debitis nulla voluptas ullam quibusdam culpa.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem eveniet modi numquam ipsam, ratione illo inventore voluptate veritatis ea, a omnis, eum consectetur ducimus. Optio voluptatibus perspiciatis corporis blanditiis nostrum?
 									Quod placeat, repellat ipsum, aliquam commodi suscipit eos quia, distinctio rem neque esse debitis consequuntur laboriosam. Consequuntur excepturi dolor nisi ipsum voluptatum ea! Ab distinctio, nihil rem nesciunt tenetur nobis.
 									Provident, veritatis. Ipsa sunt magnam rerum exercitationem illo aspernatur tenetur consequatur totam blanditiis ex repudiandae unde laboriosam ipsum fugiat minima fuga a error, facere ab ipsam ea necessitatibus architecto iure?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam maxime dolor ipsa. Ducimus, dignissimos. Fugit officia repellendus neque ducimus voluptate temporibus repudiandae quos doloribus ipsa consectetur modi, vitae, non minima!</p>
 							</div>
-							<div className={`${styles.UserProfilePostsClockDateAndButtonWrapper}`}>
-								<div className={`${styles.UserProfilePostsClockAndDateWrapper}`}>
-									<img className={`${styles.UserProfilePostsClock}`}
+							<div className={`${styles.userProfilePostsClockDateAndButtonWrapper}`}>
+								<div className={`${styles.userProfilePostsClockAndDateWrapper}`}>
+									<img className={`${styles.userProfilePostsClock}`}
 										src={'/Img/clock.svg'}
 										alt={t('Світлина мистецтва')}
 										onError={e => {
@@ -119,9 +119,48 @@ const UserProfile = () => {
 											e.target.src = '/Img/clock.svg'
 										}}
 									/>
-									<p className={`${styles.UserProfilePostsDate}`}>01.01.2022</p>
+									<p className={`${styles.userProfilePostsDate}`}>01.01.2022</p>
 								</div>
-								<button className={`${styles.UserProfilePostsButton}`}>{t('До публікації')}</button>
+								<button className={`${styles.userProfilePostsButton}`}>{t('До публікації')}</button>
+							</div>
+						</div>
+					</div>
+				</div><div className={`${styles.userProfilePostsWrapper}`}>
+					<div className={`${styles.userProfilePostsPicAndTextWrapper}`}>
+						<div className={`${styles.userProfilePostsPicWrapper}`}>
+							<img
+								className={`${styles.userProfilePostsPic}`}
+								src={'/Img/mainInstagramSliderUserPhoto.png'}
+								alt={t('Світлина мистецтва')}
+								onError={e => {
+									e.target.onerror = null
+									e.target.src = '/Img/mainPopularArtistsSlide.jpg'
+								}}
+							/>
+						</div>
+						<div className={`${styles.userProfilePostsTextWrapper}`}>
+							<div className={`${styles.userProfilePostsTitleWrapper}`}>
+								<h3 className={`${styles.userProfilePostsTitle}`}>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur omnis aperiam harum tempore voluptas reiciendis.
+								</h3>
+							</div>
+							<div className={`${styles.userProfilePostsDescriptionWrapper}`}>
+								<p className={`${styles.userProfilePostsDescription}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae id optio officiis. Nam, eius ipsam ea ipsum culpa veniam blanditiis nobis magni dignissimos a debitis, aut in iste distinctio obcaecati!
+									</p>
+							</div>
+							<div className={`${styles.userProfilePostsClockDateAndButtonWrapper}`}>
+								<div className={`${styles.userProfilePostsClockAndDateWrapper}`}>
+									<img className={`${styles.userProfilePostsClock}`}
+										src={'/Img/clock.svg'}
+										alt={t('Світлина мистецтва')}
+										onError={e => {
+											e.target.onerror = null
+											e.target.src = '/Img/clock.svg'
+										}}
+									/>
+									<p className={`${styles.userProfilePostsDate}`}>01.01.2022</p>
+								</div>
+								<button className={`${styles.userProfilePostsButton}`}>{t('До публікації')}</button>
 							</div>
 						</div>
 					</div>
