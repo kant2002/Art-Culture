d2c882a1d199aedfa35fb4aefbebbb6495369e24
Alex <71099375+SkyMantle@users.noreply.github.com>
2024-08-20 19:05:15 +0300

    =

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
@@ -6,14 +7,31 @@ const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5173
+const WP_BACKEND_URL = 'https://admin.playukraine.com/'
 
+app.use(
+	'/wp-admin',
+	createProxyMiddleware({ target: WP_BACKEND_URL, changeOrigin: true })
+)
+app.use(
+	'/wp-login.php',
+	createProxyMiddleware({ target: WP_BACKEND_URL, changeOrigin: true })
+)
+app.use(
+	'/wp-content',
+	createProxyMiddleware({ target: WP_BACKEND_URL, changeOrigin: true })
+)
+app.use(
+	'/wp-includes',
+	createProxyMiddleware({ target: WP_BACKEND_URL, changeOrigin: true })
+)
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/dist')))
+app.use(express.static(path.join(__dirname, './dist')))
 
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '/dist', 'index.html'))
+	res.sendFile(path.join(__dirname, './dist', 'index.html'))
 })
 
 app.listen(PORT, () => {
