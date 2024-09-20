c682f8f611c4167769a5fe572a052f0320759b6a
VavaDjan <vavadjan@gmail.com>
2024-09-20 17:58:37 +0300

    ...

--- src/assets/screens/MuseumPage/MuseumPage.jsx
+++ src/assets/screens/MuseumPage/MuseumPage.jsx
@@ -9,19 +9,56 @@ function MuseumPage() {
     return (
         <div className={`${styles.museumPage}`}>
 
-            <div className={`${styles.museumPageMuseumPhotoContainer}`}>
-                <img
-                    className={`${styles.museumPageMuseumPhoto}`}
-                    src={'/Img/mainPopularArtistsSlide.jpg'}
-                    alt={t('Фото митця')}
-                    onError={e => {
-                        e.target.onerror = null
-                        e.target.src = '/Img/newsCardERROR.jpg'
-                    }}
-                />
+            <div className={`${styles.museumPageNavigationContainer}`}>
+                <nav className={`${styles.museumPageNavigation}`}>
+                    <ul className={`${styles.museumPageNavigationList}`}>
+                        <li className={`${styles.museumPageNavigationItem}`}>{t('Музеї')}</li>
+                        <p className={`${styles.museumPageNavigationItemSeparator}`}>&#8250;</p>
+                        <li className={`${styles.museumPageNavigationItem}`}>{t('Назва цього музею')}</li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className={`${styles.museumPageAboutMuseumContainer}`}>
+
+                <div className={`${styles.museumPageMuseumPhotoWrapper}`}>
+                    <img
+                        className={`${styles.museumPageMuseumPhoto}`}
+                        src={'/public/Img/mainBanner.jpeg'}
+                        alt={t('Фото митця')}
+                        onError={e => {
+                            e.target.onerror = null
+                            e.target.src = '/public/Img/newsCardERROR.jpg'
+                        }}
+                    />
+                </div>
+
+                <div className={`${styles.museumPageMuseumLogoWrapper}`}>
+                    <img
+                        className={`${styles.museumPageMuseumLogo}`}
+                        src={'/public/Img/MuseumLogo.png'}
+                        alt={t('Фото митця')}
+                        onError={e => {
+                            e.target.onerror = null
+                            e.target.src = '/public/Img/newsCardERROR.jpg'
+                        }}
+                    />
+                </div>
+
+                <div className={`${styles.museumPageMuseumTitleWrapper}`}>
+                    <p className={`${styles.museumPageMuseumTitle}`}>{t('Назва цього музею')}</p>
+                </div>
+
+                <div className={`${styles.museumPageMuseumLocationWrapper}`}>
+                    <p className={`${styles.museumPageMuseumLocation}`}>Київ, Україна</p>
+                </div>
+
+                
+
             </div>
 
         </div>
+
     )
 }
 
