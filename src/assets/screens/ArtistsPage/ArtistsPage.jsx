8f1af7a306f2da422fe2a81d3c5521be55e188dd
VavaDjan <vavadjan@gmail.com>
2024-10-20 21:14:09 +0300

    !

--- src/assets/screens/ArtistsPage/ArtistsPage.jsx
+++ src/assets/screens/ArtistsPage/ArtistsPage.jsx
@@ -56,9 +56,37 @@ function ArtistsPage() {
 
                 <div className={`${styles.ArtistsPageGalleryButtonsWrapper}`}>
 
-                    <button className={`${styles.ArtistsPageGalleryButton}`}></button>
-                    <button className={`${styles.ArtistsPageGalleryButton}`}></button>
-                    <button className={`${styles.ArtistsPageGalleryButtonWhithClock}`}></button>
+                    <button className={`${styles.ArtistsPageGalleryButton}`}>
+
+                        <p className={`${styles.ArtistsPageGalleryButtonTitle}`}>{t('Усі')}</p>
+
+                    </button>
+
+                    <p className={`${styles.ArtistsPageGalleryButtonSeparator}`}>|</p>
+
+                    <button className={`${styles.ArtistsPageGalleryButton}`}>
+
+                        <p className={`${styles.ArtistsPageGalleryButtonTitle}`}>{t('А-Я')}</p>
+
+                    </button>
+
+                    <p className={`${styles.ArtistsPageGalleryButtonSeparator}`}>|</p>
+
+                    <button className={`${styles.ArtistsPageGalleryButtonWhithClock}`}>
+
+                        <p className={`${styles.ArtistsPageGalleryButtonTitle}`}>{t('Час')}</p>
+
+                        <img
+                            className={`${styles.ArtistsPageGalleryButtonClock}`}
+                            src={'/Img/clock.svg'}
+                            alt='Слідкуйте за мистецтвом!'
+                            onError={e => {
+                                e.target.onerror = null
+                                e.target.src = '/Img/newsCardERROR.jpg'
+                            }}
+                        />
+
+                    </button>
 
                 </div>
 
@@ -264,7 +292,21 @@ function ArtistsPage() {
 
                 <div className={`${styles.ArtistsPageGalleryAllArtistsButtonWrapper}`}>
 
-                    <button className={`${styles.ArtistsPageGalleryAllArtistsButton}`}>{t('Всі митці')}</button>
+                    <button className={`${styles.ArtistsPageGalleryAllArtistsButton}`}>
+
+                        <p className={`${styles.ArtistsPageGalleryAllArtistsButtonText}`}>{t('Всі митці')}</p>
+
+                        <img
+                            className={`${styles.ArtistsPageGalleryAllArtistsButtonArrow}`}
+                            src={'/public/Img/buttonArrow.svg'}
+                            alt='Слідкуйте за мистецтвом!'
+                            onError={e => {
+                                e.target.onerror = null
+                                e.target.src = '/Img/newsCardERROR.jpg'
+                            }}
+                        />
+
+                    </button>
 
                 </div>
 
