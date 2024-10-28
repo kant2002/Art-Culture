faae39b7e49d55a50fc412e1b8d343aa44b59b12
Alex <71099375+SkyMantle@users.noreply.github.com>
2024-10-28 20:04:14 +0200

    add posibility to fetcg news post creator

--- src/assets/components/Sliders/ArtistsPageSliders/ArtistsPageNewsArtistsSlider.jsx
+++ src/assets/components/Sliders/ArtistsPageSliders/ArtistsPageNewsArtistsSlider.jsx
@@ -1,36 +1,46 @@
-import React from 'react';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react'
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 
 // Import Swiper modules
 import { Navigation, Pagination } from 'swiper/modules'
 
-import '/src/styles/components/Sliders/ArtistsPageSliders/ArtistsPageNewsArtistsSlider.scss';
+import styles from '/src/styles/components/Sliders/ArtistsPageSliders/ArtistsPageNewsArtistsSlider.module.scss'
 
-const Slide = () => {
-	const { t } = useTranslation();
-	
+const Slide = ({ post, baseUrl }) => {
+	const { t, i18n } = useTranslation()
+	const currentLanguage = i18n.language
 	const navigate = useNavigate()
 
 	const handleArtistPageClick = () => {
 		navigate('/ArtistPage')
 	}
 
+	const title = currentLanguage === 'en' ? post.title_en : post.title_uk
+	const content =
+		(currentLanguage === 'en' ? post.content_en : post.content_uk) || ''
+
+	const featuredMediaUrl = post.images
+		? `${baseUrl}${post.images.replace('../../', '/')}`
+		: '/Img/halfNewsCard.jpg'
+
 	return (
-		<div className="ArtistsPageNewsArtistsSliderCardContainer">
-
-			<a className="ArtistsPageNewsArtistsSliderCardLink" onClick={handleArtistPageClick}>
-
-				<div className="ArtistsPageNewsArtistsSliderCardImgWrapper">
+		<div className={styles.ArtistsPageNewsArtistsSliderCardContainer}>
+			<a
+				className={styles.ArtistsPageNewsArtistsSliderCardLink}
+				onClick={handleArtistPageClick}
+			>
+				<div className={styles.ArtistsPageNewsArtistsSliderCardImgWrapper}>
 					<img
-						className="ArtistsPageNewsArtistsSliderCardImg"
-						src={'/Img/fullSizeRaven.jpg'}
+						className={styles.ArtistsPageNewsArtistsSliderCardImg}
+						src={featuredMediaUrl}
 						alt={t('Світлина мистецтва')}
 						onError={e => {
 							e.target.onerror = null
@@ -39,154 +49,73 @@ const Slide = () => {
 					/>
 				</div>
 
-				<div className="ArtistsPageNewsArtistsSliderCardTitleWrapper">
-					<h3 className="ArtistsPageNewsArtistsSliderCardTitle">
-						Lorem Ipsum
+				<div className={styles.ArtistsPageNewsArtistsSliderCardTitleWrapper}>
+					<h3 className={styles.ArtistsPageNewsArtistsSliderCardTitle}>
+						{title.length > 50 ? `${title.substring(0, 50)}...` : title}
 					</h3>
 				</div>
 
-				<div className="ArtistsPageNewsArtistsSliderCardDescriptionWrapper">
-					<p className="ArtistsPageNewsArtistsSliderCardDescription">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Libero vero numquam cum excepturi enim iste quis
-						doloribus cupiditate? Temporibus, laboriosam quis?
-						Reprehenderit velit sequi, inventore nostrum odit illum?
-						Doloremque, veniam.
+				<div
+					className={styles.ArtistsPageNewsArtistsSliderCardDescriptionWrapper}
+				>
+					<p className={styles.ArtistsPageNewsArtistsSliderCardDescription}>
+						{content.length > 100 ? `${content.substring(0, 100)}...` : content}
 					</p>
 				</div>
-
 			</a>
-
-		</div>
-	)
-}
-
-const Slide1 = () => {
-	const { t } = useTranslation();
-	return (
-		<div className="ArtistsPageNewsArtistsSliderCardContainer">
-
-			<div className="ArtistsPageNewsArtistsSliderCardImgWrapper">
-				<img
-					className="ArtistsPageNewsArtistsSliderCardImg"
-					src={'/Img/mainInstagramSliderIMG.jpg'}
-					alt={t('Світлина мистецтва')}
-					onError={e => {
-						e.target.onerror = null
-						e.target.src = '/public/Img/newsCardERROR.jpg'
-					}}
-				/>
-			</div>
-
-			<div className="ArtistsPageNewsArtistsSliderCardTitleWrapper">
-				<h3 className="ArtistsPageNewsArtistsSliderCardTitle">
-					Lorem Ipsum
-				</h3>
-			</div>
-
-			<div className="ArtistsPageNewsArtistsSliderCardDescriptionWrapper">
-				<p className="ArtistsPageNewsArtistsSliderCardDescription">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Libero vero numquam cum excepturi enim iste quis
-					doloribus cupiditate? Temporibus, laboriosam quis?
-					Reprehenderit velit sequi, inventore nostrum odit illum?
-					Doloremque, veniam.
-				</p>
-			</div>
-
-		</div>
-	)
-}
-
-const Slide2 = () => {
-	const { t } = useTranslation();
-	return (
-		<div className="ArtistsPageNewsArtistsSliderCardContainer">
-
-			<div className="ArtistsPageNewsArtistsSliderCardImgWrapper">
-				<img
-					className="ArtistsPageNewsArtistsSliderCardImg"
-					src={'/Img/mainPopularArtistsSlide.jpg'}
-					alt={t('Світлина мистецтва')}
-					onError={e => {
-						e.target.onerror = null
-						e.target.src = '/public/Img/newsCardERROR.jpg'
-					}}
-				/>
-			</div>
-
-			<div className="ArtistsPageNewsArtistsSliderCardTitleWrapper">
-				<h3 className="ArtistsPageNewsArtistsSliderCardTitle">
-					Lorem Ipsum
-				</h3>
-			</div>
-
-			<div className="ArtistsPageNewsArtistsSliderCardDescriptionWrapper">
-				<p className="ArtistsPageNewsArtistsSliderCardDescription">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Libero vero numquam cum excepturi enim iste quis
-					doloribus cupiditate? Temporibus, laboriosam quis?
-					Reprehenderit velit sequi, inventore nostrum odit illum?
-					Doloremque, veniam.
-				</p>
-			</div>
-
-		</div>
-	)
-}
-
-const Slide3 = () => {
-	const { t } = useTranslation();
-	return (
-		<div className="ArtistsPageNewsArtistsSliderCardContainer">
-
-			<div className="ArtistsPageNewsArtistsSliderCardImgWrapper">
-				<img
-					className="ArtistsPageNewsArtistsSliderCardImg"
-					src={'/Img/newsCard1.jpg'}
-					alt={t('Світлина мистецтва')}
-					onError={e => {
-						e.target.onerror = null
-						e.target.src = '/public/Img/newsCardERROR.jpg'
-					}}
-				/>
-			</div>
-
-			<div className="ArtistsPageNewsArtistsSliderCardTitleWrapper">
-				<h3 className="ArtistsPageNewsArtistsSliderCardTitle">
-					Lorem Ipsum
-				</h3>
-			</div>
-
-			<div className="ArtistsPageNewsArtistsSliderCardDescriptionWrapper">
-				<p className="ArtistsPageNewsArtistsSliderCardDescription">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Libero vero numquam cum excepturi enim iste quis
-					doloribus cupiditate? Temporibus, laboriosam quis?
-					Reprehenderit velit sequi, inventore nostrum odit illum?
-					Doloremque, veniam.
-				</p>
-			</div>
-
 		</div>
 	)
 }
 
 const ArtistsPageNewsArtistsSlider = () => {
-	const { t } = useTranslation();
+	const { t, i18n } = useTranslation()
+	const currentLanguage = i18n.language
+	const navigate = useNavigate()
+	const [creatorPosts, setCreatorPosts] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(null)
+
+	const host = window.location.hostname
+	const isLocalhost = host === 'localhost' || host === '127.0.0.1'
+	const baseUrl = isLocalhost
+		? 'http://localhost:5000'
+		: 'https://art.playukraine.com'
+
+	useEffect(() => {
+		const fetchCreatorPosts = async () => {
+			try {
+				const response = await axios.get('/api/posts/creators')
+				console.log('Received creator posts:', response.data)
+				setCreatorPosts(response.data.posts || [])
+				setLoading(false)
+			} catch (err) {
+				console.error('Error fetching creator posts:', err)
+				setError(t('Не вдалося завантажити публікації.'))
+				setLoading(false)
+			}
+		}
+
+		fetchCreatorPosts()
+	}, [t])
+
 	return (
-		<div className="ArtistsPageNewsArtistsSliderContainer">
-			<div className="ArtistsPageNewsArtistsSliderWrapper">
-				<div className="ArtistsPageNewsArtistsSliderTopInnerWrapper">
-					<div className="ArtistsPageNewsArtistsSliderTitleWrapper">
-						<p className="ArtistsPageNewsArtistsSliderTitle">
+		<div className={styles.ArtistsPageNewsArtistsSliderContainer}>
+			<div className={styles.ArtistsPageNewsArtistsSliderWrapper}>
+				<div className={styles.ArtistsPageNewsArtistsSliderTopInnerWrapper}>
+					<div className={styles.ArtistsPageNewsArtistsSliderTitleWrapper}>
+						<p className={styles.ArtistsPageNewsArtistsSliderTitle}>
 							{t('Новини.')} &#8243;{t('Митці')}&#8243;
 						</p>
 					</div>
-					<div className="ArtistsPageNewsArtistsSliderLikeAndShareWrapper">
-						<div className="ArtistsPageNewsArtistsSliderLikeInnerWrapper">
-							<button className="ArtistsPageNewsArtistsSliderLikeButton">
-								<img className="ArtistsPageNewsArtistsSliderLikeButtonImg"
+					<div
+						className={styles.ArtistsPageNewsArtistsSliderLikeAndShareWrapper}
+					>
+						<div
+							className={styles.ArtistsPageNewsArtistsSliderLikeInnerWrapper}
+						>
+							<button className={styles.ArtistsPageNewsArtistsSliderLikeButton}>
+								<img
+									className={styles.ArtistsPageNewsArtistsSliderLikeButtonImg}
 									src={'/Img/likeHeart.svg'}
 									alt={t('Світлина вподобайки')}
 									onError={e => {
@@ -196,9 +125,14 @@ const ArtistsPageNewsArtistsSlider = () => {
 								/>
 							</button>
 						</div>
-						<div className="ArtistsPageNewsArtistsSliderShareInnerWrapper">
-							<button className="ArtistsPageNewsArtistsSliderShareButtonButton">
-								<img className="ArtistsPageNewsArtistsSliderShareButtonImg"
+						<div
+							className={styles.ArtistsPageNewsArtistsSliderShareInnerWrapper}
+						>
+							<button
+								className={styles.ArtistsPageNewsArtistsSliderShareButtonButton}
+							>
+								<img
+									className={styles.ArtistsPageNewsArtistsSliderShareButtonImg}
 									src={'/Img/shareArrow.svg'}
 									alt={t('Світлина поширити')}
 									onError={e => {
@@ -210,37 +144,37 @@ const ArtistsPageNewsArtistsSlider = () => {
 						</div>
 					</div>
 				</div>
-				<div className="ArtistsPageNewsArtistsSliderBottomInnerWrapper">
+				<div className={styles.ArtistsPageNewsArtistsSliderBottomInnerWrapper}>
 					<Swiper
 						modules={[Navigation, Pagination]}
 						spaceBetween={0}
-						slidesPerView={'auto'}
+						slidesPerView={'4'}
 						navigation
 						pagination={{ clickable: false, type: 'fraction' }}
 						onSlideChange={() => console.log('slide change')}
-						onSwiper={(swiper) => console.log(swiper)}
+						onSwiper={swiper => console.log(swiper)}
 					>
-						<SwiperSlide>
-							<Slide />
-						</SwiperSlide>
-						<SwiperSlide>
-							<Slide1 />
-						</SwiperSlide>
-						<SwiperSlide>
-							<Slide2 />
-						</SwiperSlide>
-						<SwiperSlide>
-							<Slide3 />
-						</SwiperSlide>
-						<SwiperSlide>
-							<Slide />
-						</SwiperSlide>
-						<SwiperSlide>
-							<Slide />
-						</SwiperSlide>
-						<SwiperSlide>
-							<Slide />
-						</SwiperSlide>
+						{loading ? (
+							<SwiperSlide>
+								<div className={styles.loading}>{t('Завантаження...')}</div>
+							</SwiperSlide>
+						) : error ? (
+							<SwiperSlide>
+								<div className={styles.error}>{error}</div>
+							</SwiperSlide>
+						) : creatorPosts.length === 0 ? (
+							<SwiperSlide>
+								<div className={styles.noPosts}>
+									{t('Немає публікацій від митців.')}
+								</div>
+							</SwiperSlide>
+						) : (
+							creatorPosts.map(post => (
+								<SwiperSlide key={post.id}>
+									<Slide post={post} baseUrl={baseUrl} />
+								</SwiperSlide>
+							))
+						)}
 					</Swiper>
 					<div className={'${swiper-button-prev}'}></div>
 					<div className={'${swiper-pagination}'}></div>
@@ -251,4 +185,4 @@ const ArtistsPageNewsArtistsSlider = () => {
 	)
 }
 
-export default ArtistsPageNewsArtistsSlider;
+export default ArtistsPageNewsArtistsSlider
