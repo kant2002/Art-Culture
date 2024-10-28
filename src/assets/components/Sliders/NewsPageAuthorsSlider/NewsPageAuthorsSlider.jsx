4093c88e3f75fedcc744881fa4ae7588d03835b1
Alex <71099375+SkyMantle@users.noreply.github.com>
2024-10-28 14:05:10 +0200

    slider author updated

--- src/assets/components/Sliders/NewsPageAuthorsSlider/NewsPageAuthorsSlider.jsx
+++ src/assets/components/Sliders/NewsPageAuthorsSlider/NewsPageAuthorsSlider.jsx
@@ -1,126 +1,3 @@
-// import axios from 'axios'
-// import React, { useEffect, useState } from 'react'
-// import { useTranslation } from 'react-i18next'
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from 'swiper/react'
-// // Import Swiper styles
-// import 'swiper/css'
-// import 'swiper/css/navigation'
-// import 'swiper/css/pagination'
-
-// // Import Swiper modules
-// import { Navigation, Pagination } from 'swiper/modules'
-
-// import '/src/styles/components/Sliders/NewsPageAuthorsSlider/NewsPageAuthorsSlider.scss'
-
-// const Slide = ({ post }) => {
-//     const { t } = useTranslation()
-//     return (
-//         <div className='newsPageAuthorsSliderCardContainer'>
-//             <div className='newsPageAuthorsSliderCardWrapper'>
-//                 <div className='newsPageAuthorsSliderCardUserPhotoWrapper'>
-//                     <img
-//                         className='newsPageAuthorsSliderCardUserPhoto'
-//                         src={'/Img/mainInstagramSliderUserPhoto.png'}
-//                         alt={t('Фотографія автора')}
-//                         onError={e => {
-//                             e.target.onerror = null
-//                             e.target.src = '/Img/mainInstagramSliderUserPhoto.png'
-//                         }}
-//                     />
-//                 </div>
-//                 <div className='newsPageAuthorsSliderCardUserNameWrapper'>
-//                     <p className='newsPageAuthorsSliderCardUserName'>Дмитро Рудченко</p>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// const NewsPageAuthorsSlider = () => {
-//     const { t } = useTranslation()
-//     const [posts, setPosts] = useState([])
-//     const [error, setError] = useState(null)
-
-//     useEffect(() => {
-//         const fetchInstagramPosts = async () => {
-//             try {
-//                 const response = await axios.get(``)
-//                 setPosts(response.data.data)
-//             } catch (error) {
-//                 console.error('Error fetching Instagram post data', error)
-//                 setError(error)
-//             }
-//         }
-
-//         fetchInstagramPosts()
-//     }, [])
-
-//     return (
-//         <div className='newsPageAuthorsSliderContainer'>
-//             <div className='newsPageAuthorsSliderWrapper'>
-//                 <div className='newsPageAuthorsSliderTitleWrapper'>
-//                     <p className='newsPageAuthorsSliderTitle'>{t('Автори')}</p>
-//                 </div>
-
-//                 <div className='newsPageAuthorsSliderInnerWrapper'>
-//                     <Swiper
-//                         modules={[Navigation, Pagination]}
-//                         spaceBetween={0}
-//                         slidesPerView={'auto'}
-//                         navigation
-//                         pagination={{ clickable: false, type: 'fraction' }}
-//                         onSlideChange={() => console.log('slide change')}
-//                         onSwiper={swiper => console.log(swiper)}
-//                     >
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                         <SwiperSlide>
-//                             <Slide />
-//                         </SwiperSlide>
-//                     </Swiper>
-//                     <div className={'${swiper-button-prev}'}></div>
-//                     <div className={'${swiper-pagination}'}></div>
-//                     <div className={'${swiper-button-next}'}></div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default NewsPageAuthorsSlider
-
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
@@ -139,6 +16,11 @@ import '/src/styles/components/Sliders/NewsPageAuthorsSlider/NewsPageAuthorsSlid
 
 const Slide = ({ creator, onClick }) => {
 	const { t } = useTranslation()
+	const host = window.location.hostname
+	const isLocalhost = host === 'localhost' || host === '127.0.0.1'
+	const baseUrl = isLocalhost
+		? 'http://localhost:5000'
+		: 'https://art.playukraine.com'
 	return (
 		<div
 			className='newsPageAuthorsSliderCardContainer'
@@ -150,10 +32,7 @@ const Slide = ({ creator, onClick }) => {
 						className='newsPageAuthorsSliderCardUserPhoto'
 						src={
 							creator.images
-								? // ? `http://localhost:5000${creator.images.replace('../../', '/')}`
-									// : '/Img/mainInstagramSliderUserPhoto.png'
-
-									`https://art.playukraine.com${creator.images.replace('../../', '/')}`
+								? `${baseUrl}${creator.images.replace('../../', '/')}`
 								: '/Img/halfNewsCard.jpg'
 						}
 						alt={t('Фотографія автора')}
