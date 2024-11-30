8693d7f9697f5b70be59a2d92c3569d08881e6e7
Andrii Kurdiumov <kant2002@gmail.com>
2024-11-30 21:51:27 +0600

    Add boilerplate dispaying art terms starting with specific letters (#150)

--- server/src/controllers/artTermsController.js
+++ server/src/controllers/artTermsController.js
@@ -4,11 +4,10 @@ export const getArtTermsByLang = async (req, res, next) => {
 	try {
 		const lang = req.params.lang ?? "uk"
 		if (lang != "uk" && lang != "en") {
-			return res.status(400).json({ error: 'invalid creator id' })
+			return res.status(400).json({ error: 'invalid language' })
 		}
 
         const orderBy = lang === "uk" ? { title_uk: 'asc' } : { title_en: 'asc' };
-        console.log(prisma);
 		const artTerms = await prisma.artTerm.findMany({
             orderBy: orderBy,
             select: {
@@ -36,6 +35,40 @@ export const getArtTermsByLang = async (req, res, next) => {
         })
 
 		res.json({ artTerms: firstTerms })
+	} catch (error) {
+		console.error('Error fetch data art-term id', error)
+		next(error)
+	}
+}
+
+export const getArtTermsByLetter = async (req, res, next) => {
+	try {
+		let letter = req.params.letter
+		if (!letter) {
+			return res.status(400).json({ error: 'invalid letter' })
+		}
+
+        letter = letter[0]
+        const orderBy = letter === "uk" ? { title_uk: 'asc' } : { title_en: 'asc' };
+		const artTerms = await prisma.artTerm.findMany({
+            orderBy: orderBy,
+            where: {
+                OR: [ 
+                    {
+                        title_uk: {
+                            startsWith: letter
+                        }
+                    },
+                    {
+                        title_en: {
+                            startsWith: letter
+                        }
+                    },
+                ]
+            }
+        });
+
+		res.json({ artTerms })
 	} catch (error) {
 		console.error('Error fetch data art-term id', error)
 		next(error)
