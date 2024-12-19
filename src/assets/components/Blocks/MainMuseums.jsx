0143fbc9cd27980d9142aa3b054e2662290e730b
VavaDjan <vavadjan@gmail.com>
2024-12-19 23:06:19 +0200

    ...процесс

--- src/assets/components/Blocks/MainMuseums.jsx
+++ src/assets/components/Blocks/MainMuseums.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 import styles from '/src/styles/components/Blocks/MainNews.module.scss'
