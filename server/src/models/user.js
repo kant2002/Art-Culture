58e56b68854e253cf41c35673915c9f784e100b2
Alex <71099375+SkyMantle@users.noreply.github.com>
2025-01-17 10:15:09 +0200

    -

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,21 +1,28 @@
-import prisma from '../../prismaClient'
+import prisma from "../../prismaClient"
 
 const userSchema = new prisma.Schema(
-	{
-		email: { type: String, required: true, unique: true },
-		password: { type: String, required: true },
-		role: {
-			type: String,
-			enum: ['USER', 'CREATOR', 'MUSEUM', 'EDITOR'],
-			default: 'USER',
-		},
-		title: { type: String, default: '' },
-		bio: { type: String, default: '' },
-		images: { type: String, default: null }, // Path to profile image
-	},
-	{ timestamps: true }
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: [
+        "USER",
+        "CREATOR",
+        "MUSEUM",
+        "EDITOR",
+        "AUTHOR",
+        "EXHIBITION_OWNER",
+      ],
+      default: "USER",
+    },
+    title: { type: String, default: "" },
+    bio: { type: String, default: "" },
+    images: { type: String, default: null }, // Path to profile image
+  },
+  { timestamps: true },
 )
 
-const User = prisma.model('User', userSchema)
+const User = prisma.model("User", userSchema)
 
 export default User
