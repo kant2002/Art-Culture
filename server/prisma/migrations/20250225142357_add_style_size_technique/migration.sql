6330677c46874af769ea388e53778494dad8e447
Alex <71099375+SkyMantle@users.noreply.github.com>
2025-02-25 16:28:58 +0200

    Add size, style, and technique fields to Product model

---
+++ server/prisma/migrations/20250225142357_add_style_size_technique/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE `Product` ADD COLUMN `size` VARCHAR(100) NULL,
+    ADD COLUMN `style_en` VARCHAR(100) NULL,
+    ADD COLUMN `style_uk` VARCHAR(100) NULL,
+    ADD COLUMN `technique_en` VARCHAR(100) NULL,
+    ADD COLUMN `technique_uk` VARCHAR(100) NULL;
