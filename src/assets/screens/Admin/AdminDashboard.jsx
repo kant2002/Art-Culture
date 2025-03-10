21820e6f5371c57d634392d81630ea168f8f4daf
VavaDjan <vavadjan@gmail..com>
2025-03-10 20:37:07 +0200

    Добавить стили и переводы для новых секций в панели управления администратора

--- src/assets/screens/Admin/AdminDashboard.jsx
+++ src/assets/screens/Admin/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 import ProfilePageContainer from '@components/Blocks/ProfilePageContainer'
+import styles from '@styles/components/UserProfile/AdminDashboard.module.scss'
 
 const AdminDashboard = () => {
 	const { t } = useTranslation()
@@ -8,8 +9,19 @@ const AdminDashboard = () => {
 
 	return (
 		<ProfilePageContainer>
-			<div>
-				<h2>{t('Панель керування')}</h2>				
+			<h2 className={`${styles.AdminDashboardTitle}`}>
+				{t('Панель керування')}
+			</h2>
+			<div className={`${styles.AdminDashboardDemandARelookContainer}`}>
+				<h3 className={`${styles.AdminDashboardDemandARelookTitle}`}>
+					{t('Потребують перегляду')}
+				</h3>
+			</div>
+			<div
+				className={`${styles.AdminDashboardNewUsersContainer}`}>
+				<h3 className={`${styles.AdminDashboardDemandARelookTitle}`}>
+					{t('Нові користувачі')}
+				</h3>
 			</div>
 		</ProfilePageContainer>
 	)
