9ac105472f5e800fc188ce0ee91f5c6e582c54ac
VavaDjan <vavadjan@gmail..com>
2025-03-10 21:40:33 +0200

    Добавить новый контейнер статистики и переводы для карточек в панели управления администратора

--- src/assets/screens/Admin/AdminDashboard.jsx
+++ src/assets/screens/Admin/AdminDashboard.jsx
@@ -12,16 +12,30 @@ const AdminDashboard = () => {
 			<h2 className={`${styles.AdminDashboardTitle}`}>
 				{t('Панель керування')}
 			</h2>
-			<div className={`${styles.AdminDashboardDemandARelookContainer}`}>
-				<h3 className={`${styles.AdminDashboardDemandARelookTitle}`}>
-					{t('Потребують перегляду')}
-				</h3>
-			</div>
-			<div
-				className={`${styles.AdminDashboardNewUsersContainer}`}>
-				<h3 className={`${styles.AdminDashboardDemandARelookTitle}`}>
-					{t('Нові користувачі')}
-				</h3>
+			<div className={`${styles.AdminDashboardStatisticsContainer}`}>
+				<div className={`${styles.AdminDashboardDemandARelookContainer}`}>
+					<h3 className={`${styles.AdminDashboardDemandARelookTitle}`}>
+						{t('Потребують перегляду')}
+					</h3>
+					<div className={`${styles.AdminDashboardDemandARelookPostsContainer}`}>
+						<div className={`${styles.AdminDashboardDemandARelookNewsContainer}`}>
+							<h3 className={`${styles.AdminDashboardDemandARelookNewsTitle}`}>
+								{t('Новини')}
+							</h3>
+						</div>
+						<div className={`${styles.AdminDashboardDemandARelookCardsContainer}`}>
+							<h3 className={`${styles.AdminDashboardDemandARelookCardsTitle}`}>
+								{t('Карточки')}
+							</h3>
+						</div>
+					</div>
+				</div>
+				<div
+					className={`${styles.AdminDashboardNewUsersContainer}`}>
+					<h3 className={`${styles.AdminDashboardDemandARelookTitle}`}>
+						{t('Нові користувачі')}
+					</h3>
+				</div>
 			</div>
 		</ProfilePageContainer>
 	)
