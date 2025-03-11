bd8b5c80cdb0efdd7ab0db97c0cb0b301e08113b
VavaDjan <vavadjan@gmail..com>
2025-03-11 20:21:02 +0200

    Remake DASHBOARD

--- src/assets/screens/Admin/AdminDashboard.jsx
+++ src/assets/screens/Admin/AdminDashboard.jsx
@@ -12,29 +12,48 @@ const AdminDashboard = () => {
 			<h2 className={`${styles.AdminDashboardTitle}`}>
 				{t('Панель керування')}
 			</h2>
+			<div className={`${styles.AdminDashboardDemandARelookContainer}`}>
+				<h3 className={`${styles.AdminDashboardDemandARelookTitle}`}>
+					{t('Потребують перегляду')}
+				</h3>
+			</div>
 			<div className={`${styles.AdminDashboardStatisticsContainer}`}>
-				<div className={`${styles.AdminDashboardDemandARelookContainer}`}>
-					<h3 className={`${styles.AdminDashboardDemandARelookTitle}`}>
-						{t('Потребують перегляду')}
+				<div className={`${styles.AdminDashboardDemandARelookNewsContainer}`}>
+					<h3 className={`${styles.AdminDashboardDemandARelookNewsTitle}`}>
+						{t('Новини')}
 					</h3>
-					<div className={`${styles.AdminDashboardDemandARelookPostsContainer}`}>
-						<div className={`${styles.AdminDashboardDemandARelookNewsContainer}`}>
-							<h3 className={`${styles.AdminDashboardDemandARelookNewsTitle}`}>
-								{t('Новини')}
-							</h3>
-						</div>
-						<div className={`${styles.AdminDashboardDemandARelookCardsContainer}`}>
-							<h3 className={`${styles.AdminDashboardDemandARelookCardsTitle}`}>
-								{t('Карточки')}
-							</h3>
-						</div>
-					</div>
+					<h3 className={`${styles.AdminDashboardDemandARelookNewsNumber}`}>1</h3>
+					<button
+						className={`${styles.AdminDashboardDemandARelookNewsButton}`}
+						// onClick={() => navigate('/admin/news')}
+					>
+						{t('Переглянути')}
+					</button>
+				</div>
+				<div className={`${styles.AdminDashboardDemandARelookCardsContainer}`}>
+					<h3 className={`${styles.AdminDashboardDemandARelookCardsTitle}`}>
+						{t('Карточки')}
+					</h3>
+					<h3 className={`${styles.AdminDashboardDemandARelookNewsNumber}`}>1</h3>
+					<button
+						className={`${styles.AdminDashboardDemandARelookNewsButton}`}
+					// onClick={() => navigate('/admin/news')}
+					>
+						{t('Переглянути')}
+					</button>
 				</div>
 				<div
 					className={`${styles.AdminDashboardNewUsersContainer}`}>
 					<h3 className={`${styles.AdminDashboardDemandARelookTitle}`}>
 						{t('Нові користувачі')}
 					</h3>
+					<h3 className={`${styles.AdminDashboardDemandARelookNewsNumber}`}>1</h3>
+					<button
+						className={`${styles.AdminDashboardDemandARelookNewsButton}`}
+					// onClick={() => navigate('/admin/news')}
+					>
+						{t('Переглянути')}
+					</button>
 				</div>
 			</div>
 		</ProfilePageContainer>
