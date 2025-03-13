b13a979e7c90e675903643e1e1f023205adedcae
VavaDjan <vavadjan@gmail..com>
2025-03-13 10:55:30 +0200

    Обновить панель администратора: улучшить отображение статистики с использованием динамического рендеринга и скрыть элементы с нулевыми значениями

--- src/assets/screens/Admin/AdminDashboard.jsx
+++ src/assets/screens/Admin/AdminDashboard.jsx
@@ -1,3 +1,114 @@
+// import ProfilePageContainer from '@components/Blocks/ProfilePageContainer'
+// import styles from '@styles/components/UserProfile/AdminDashboard.module.scss'
+// import axios from 'axios'
+// import { useEffect, useState } from 'react'
+// import { useTranslation } from 'react-i18next'
+// import { useNavigate } from 'react-router-dom'
+
+// const AdminDashboard = () => {
+// 	const { t } = useTranslation()
+// 	const navigate = useNavigate()
+
+// 	const [pendingCounts, setPendingCounts] = useState({
+// 		posts: 0,
+// 		products: 0,
+// 		users: 0,
+// 	})
+
+// 	useEffect(() => {
+// 		const fetchPendingCounts = async () => {
+// 			try {
+// 				const { data } = await axios.get('/api/admin/pending-counts', {
+// 					headers: {
+// 						Authorization: `Bearer ${localStorage.getItem('token')}`,
+// 					},
+// 				})
+// 				setPendingCounts(data)
+// 			} catch (error) {
+// 				console.error(error)
+// 			}
+// 		}
+// 		fetchPendingCounts()
+// 	}, [])
+
+// 	return (
+// 		<ProfilePageContainer>
+// 			<h2 className={`${styles.AdminDashboardTitle}`}>
+// 				{t('Панель керування')}
+// 			</h2>
+// 			<div className={`${styles.AdminDashboardDemandARelookContainer}`}>
+// 				<h3 className={`${styles.AdminDashboardDemandARelookTitle}`}>
+// 					{t('Потребують перегляду')}
+// 				</h3>
+// 			</div>
+// 			<div className={`${styles.AdminDashboardStatisticsContainer}`}>
+// 				<div
+// 					className={`${styles.AdminDashboardStatisticsItemContainer}`}
+// 				>
+// 					<h3
+// 						className={`${styles.AdminDashboardStatisticsItemTitle}`}
+// 					>
+// 						{t('Новини')}
+// 					</h3>
+// 					<h3
+// 						className={`${styles.AdminDashboardStatisticsItemValue}`}
+// 					>
+// 						{pendingCounts.posts}
+// 					</h3>
+// 					<button
+// 						className={`${styles.AdminDashboardDemandARelookButton}`}
+// 						// onClick={() => navigate('/admin/news')}
+// 					>
+// 						{t('Переглянути')}
+// 					</button>
+// 				</div>
+// 				<div
+// 					className={`${styles.AdminDashboardStatisticsItemContainer}`}
+// 				>
+// 					<h3
+// 						className={`${styles.AdminDashboardStatisticsItemTitle}`}
+// 					>
+// 						{t('Карточки')}
+// 					</h3>
+// 					<h3
+// 						className={`${styles.AdminDashboardStatisticsItemValue}`}
+// 					>
+// 						{pendingCounts.products}
+// 					</h3>
+// 					<button
+// 						className={`${styles.AdminDashboardDemandARelookButton}`}
+// 						// onClick={() => navigate('/admin/news')}
+// 					>
+// 						{t('Переглянути')}
+// 					</button>
+// 				</div>
+// 				<div
+// 					className={`${styles.AdminDashboardStatisticsItemContainer}`}
+// 				>
+// 					<h3
+// 						className={`${styles.AdminDashboardStatisticsItemTitle}`}
+// 					>
+// 						{t('Нові користувачі')}
+// 					</h3>
+// 					<h3
+// 						className={`${styles.AdminDashboardStatisticsItemValue}`}
+// 					>
+// 						1
+// 					</h3>
+// 					<button
+// 						className={`${styles.AdminDashboardDemandARelookButton}`}
+// 						// onClick={() => navigate('/admin/news')}
+// 					>
+// 						{t('Переглянути')}
+// 					</button>
+// 				</div>
+// 			</div>
+// 		</ProfilePageContainer>
+// 	)
+// }
+
+// export default AdminDashboard
+
 import ProfilePageContainer from '@components/Blocks/ProfilePageContainer'
 import styles from '@styles/components/UserProfile/AdminDashboard.module.scss'
 import axios from 'axios'
@@ -33,75 +144,44 @@ const AdminDashboard = () => {
 
 	return (
 		<ProfilePageContainer>
-			<h2 className={`${styles.AdminDashboardTitle}`}>
+			<h2 className={styles.AdminDashboardTitle}>
 				{t('Панель керування')}
 			</h2>
-			<div className={`${styles.AdminDashboardDemandARelookContainer}`}>
-				<h3 className={`${styles.AdminDashboardDemandARelookTitle}`}>
+			<div className={styles.AdminDashboardDemandARelookContainer}>
+				<h3 className={styles.AdminDashboardDemandARelookTitle}>
 					{t('Потребують перегляду')}
 				</h3>
 			</div>
-			<div className={`${styles.AdminDashboardStatisticsContainer}`}>
-				<div
-					className={`${styles.AdminDashboardStatisticsItemContainer}`}
-				>
-					<h3
-						className={`${styles.AdminDashboardStatisticsItemTitle}`}
-					>
-						{t('Новини')}
-					</h3>
-					<h3
-						className={`${styles.AdminDashboardStatisticsItemValue}`}
-					>
-						{pendingCounts.posts}
-					</h3>
-					<button
-						className={`${styles.AdminDashboardDemandARelookButton}`}
-						// onClick={() => navigate('/admin/news')}
-					>
-						{t('Переглянути')}
-					</button>
-				</div>
-				<div
-					className={`${styles.AdminDashboardStatisticsItemContainer}`}
-				>
-					<h3
-						className={`${styles.AdminDashboardStatisticsItemTitle}`}
-					>
-						{t('Карточки')}
-					</h3>
-					<h3
-						className={`${styles.AdminDashboardStatisticsItemValue}`}
-					>
-						{pendingCounts.products}
-					</h3>
-					<button
-						className={`${styles.AdminDashboardDemandARelookButton}`}
-						// onClick={() => navigate('/admin/news')}
-					>
-						{t('Переглянути')}
-					</button>
-				</div>
-				<div
-					className={`${styles.AdminDashboardStatisticsItemContainer}`}
-				>
-					<h3
-						className={`${styles.AdminDashboardStatisticsItemTitle}`}
-					>
-						{t('Нові користувачі')}
-					</h3>
-					<h3
-						className={`${styles.AdminDashboardStatisticsItemValue}`}
-					>
-						1
-					</h3>
-					<button
-						className={`${styles.AdminDashboardDemandARelookButton}`}
-						// onClick={() => navigate('/admin/news')}
-					>
-						{t('Переглянути')}
-					</button>
-				</div>
+			<div className={styles.AdminDashboardStatisticsContainer}>
+				{[
+					{ key: 'posts', label: t('Новини') },
+					{ key: 'products', label: t('Карточки') },
+					{ key: 'users', label: t('Нові користувачі') },
+				].map(({ key, label }) => {
+					const value = pendingCounts[key] ?? 0 // Если undefined или null, приравниваем к 0
+					return (
+						<div
+							key={key}
+							className={styles.AdminDashboardStatisticsItemContainer}
+						>
+							<h3 className={styles.AdminDashboardStatisticsItemTitle}>
+								{label}
+							</h3>
+							<h3
+								className={styles.AdminDashboardStatisticsItemValue}
+								style={{ visibility: value === 0 ? 'hidden' : 'visible' }}
+							>
+								{value}
+							</h3>
+							<button
+								className={styles.AdminDashboardDemandARelookButton}
+								style={{ visibility: value === 0 ? 'hidden' : 'visible' }}
+							>
+								{t('Переглянути')}
+							</button>
+						</div>
+					)
+				})}
 			</div>
 		</ProfilePageContainer>
 	)
