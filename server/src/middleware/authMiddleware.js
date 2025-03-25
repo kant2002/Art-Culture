09f1ab62a92d58d970d9bf67c0cec44dfaee96fd
Andrii Kurdiumov <kant2002@gmail.com>
2025-03-25 22:06:50 +0500

    Remove code (#322)

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -8,16 +8,13 @@ const authenticateToken = async (req, res, next) => {
   const token = authHeader && authHeader.split(" ")[1]
 
   if (!authHeader || !token) {
-    console.error("Missing authorization header or token.")
     return res.status(401).json({ error: "Access token is missing" })
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    console.log("Decoded token:", decoded)
 
     if (!decoded || !decoded.id) {
-      console.error("Invalid token structure.")
       return res.status(400).json({ error: "Invalid token structure" })
     }
 
@@ -45,17 +42,14 @@ const authenticateToken = async (req, res, next) => {
     })
 
     if (!user) {
-      console.error("User not found.")
       return res.status(404).json({ error: "User not found" })
     }
 
     req.user = user // Attach the full user object to req.user
     next()
   } catch (error) {
-    console.error("Auth error:", error)
     return res.status(403).json({ error: "Invalid token" })
   }
-  console.log("authenticateToken - req.user:", req.user)
 }
 
 export default authenticateToken
